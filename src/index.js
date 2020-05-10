78bd00684e3f107e6d99e4584d21238941f3cb10
helloworldchao <helloworldchao@outlook.com>
2020-05-10 15:23:20 +0800

    feat: 优化图标和tray点击

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, Menu, Tray, nativeImage } = require('electron');
 const path = require('path');
 
+const iconPath = path.join(__dirname, '../static/icon.png');
+
 // 使用全局变量防止被系统垃圾回收
 let win = null;
 let applicationMenu = null;
@@ -40,6 +42,7 @@ function setApplicationMenu() {
 
 function createWindow () {   
   win = new BrowserWindow({
+    icon: iconPath,
     width: 1000,
     height: 680,
     resizable: false,
@@ -76,12 +79,15 @@ function removeTip() {
 }
 
 function initTray() {
-  const icon = nativeImage.createFromPath(path.join(__dirname, '../static/icon.png'));
+  const icon = nativeImage.createFromPath(iconPath);
   tray = new Tray(icon);
+
 
   const openWindow = function() {
     win.show();
   }
+
+  tray.addListener('click', openWindow);
 
   const contextMenu = Menu.buildFromTemplate([
     { label: '打开主界面', type: 'normal', click: openWindow },
